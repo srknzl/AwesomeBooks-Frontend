ab47c440a218dc9c0c0ae83cc3fa4921bf8fede6
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-03-30 14:18:16 +0300

    Add sitemap.xml

--- src/router/index.js
+++ src/router/index.js
@@ -96,7 +96,7 @@ router.beforeEach(async (to, from, next) => {
     }                                                                                                                                                                                                                                                                                                                                     
     if (!store.state.loggedIn) {
       next({
-        path: '/login',
+        path: '/giris',
         query: { redirect: to.fullPath }
       });
     } else {
@@ -106,6 +106,32 @@ router.beforeEach(async (to, from, next) => {
     next(); // make sure to always call next()!
   }
 });
+function getRoutesList(routes, pre) {
+  return routes.reduce((array, route) => {
+    const path = `${pre}${route.path}`;
 
+    if (route.path !== "*") {
+      array.push(path);
+    }
+
+    if (route.children) {
+      array.push(...getRoutesList(route.children, `${path}/`));
+    }
+
+    return array;
+  }, []);
+}
+function getRoutesXML() {
+  const list = getRoutesList(
+    router.options.routes,
+    "https://awesomebook.store"
+  )
+    .map(route => `<url><loc>${route}</loc></url>`)
+    .join("\r\n");
+  return `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    ${list}
+  </urlset>`;
+}
+console.log(getRoutesXML());
 
 export default router;
